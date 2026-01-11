1d1820c79197d582cf2aa7ea03d70ab0697fc260
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-11 23:22:40 +0545

    learning about the class

--- simpleNote.java
+++ simpleNote.java
@@ -3,6 +3,26 @@ package OOPS_Concept;
 public class simpleNote {
     public static void main(String [] arg) {
 
+/*
+OOPS = Object-Oriented Programming System
+Goal: write clean, reusable, scalable code instead of messy logic.
+
+--------------------------------------------------
+1. CLASS
+--------------------------------------------------
+- Blueprint / template
+- Does NOT occupy memory
+- Defines properties (variables) and behaviors (methods)
+
+Example:
+class Student {
+    int id;
+    String name;
+
+    void study() { }
+
+}*/
+
     }
 }
 
