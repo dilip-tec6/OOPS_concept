bfcf84c8abdf9db6e701d42f05dc47794f494a8c
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-16 21:49:44 +0545

    Basic syntax of Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+package OOPS_Concept;
+class Shape{
+    String color;
+
+}
+class Triangle extends Shape{
+
+}
+public class Inheritance {
+    public static void main(String[]arg) {
+        Triangle t1 = new Triangle();
+        t1.color= "red";
+        //There are 4 Types of Inheritance in java
+    }
+}
---
+++ Polymorphism.java
@@ -0,0 +1,4 @@
+package OOPS_Concept;
+
+public class Polymorphism {
+}
