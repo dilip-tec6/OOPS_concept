8551f1c6021861eef79cdc79d1e61db269c5e9d5
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-17 21:18:16 +0545

    Program: Inheritance and Polymorphism in Java

---
+++ AnimalPolymorphism.java
@@ -0,0 +1,23 @@
+package OOPS_Concept;
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class AnimalPolymorphism {
+
+}
